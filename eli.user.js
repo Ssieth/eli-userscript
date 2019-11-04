453b653a76493219258d034a504b713c7c61047a
Ssieth <github@ssieth.co.uk>
2019-11-04 23:06:42 +0000

    Slight tweak

--- eli.user.js
+++ eli.user.js
@@ -8,7 +8,7 @@
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
 // @require     https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js
 // @require     https://cdnjs.cloudflare.com/ajax/libs/jquery.tablesorter/2.31.0/js/jquery.tablesorter.min.js
-// @version     1.40.1
+// @version     1.40.1 
 // @grant       GM_getValue
 // @grant       GM_setValue
 // @grant       GM_deleteValue
