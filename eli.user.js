8bd6cf7c24aea89e3a82a0ec64f797394670d8cb
Ssieth <github@ssieth.co.uk>
2020-04-08 18:51:19 +0100

    Update because a FireFox update broke things

--- eli.user.js
+++ eli.user.js
@@ -8,7 +8,7 @@
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
 // @require     https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js
 // @require     https://cdnjs.cloudflare.com/ajax/libs/jquery.tablesorter/2.31.0/js/jquery.tablesorter.min.js
-// @version     1.40.1 
+// @version     1.40.2 
 // @grant       GM_getValue
 // @grant       GM_setValue
 // @grant       GM_deleteValue
@@ -225,7 +225,7 @@ function rInt(min, max) {
 function log(strLogTag, strMessage) {
   if (logTags[strLogTag]) {
     var now = new Date();
-    if (log.caller === undefined) {
+    if (log.caller === undefined || log.caller === null) {
       console.log(now.toLocaleString() + ":" + strLogTag + ": " + log.caller.name + ": " + JSON.stringify(strMessage));
     }
     else {
