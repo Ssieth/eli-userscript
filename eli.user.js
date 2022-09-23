727dd8eeb0b43eab7603a2c8a14858030b2da730
Ssieth <github@ssieth.co.uk>
2022-09-23 17:53:05 +0100

    V1.46.3 This time for realsies

--- eli.user.js
+++ eli.user.js
@@ -7,7 +7,7 @@
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
 // @require     https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js
 // @require     https://cdnjs.cloudflare.com/ajax/libs/jquery.tablesorter/2.31.0/js/jquery.tablesorter.min.js
-// @version     1.46.2
+// @version     1.46.3
 // @grant       GM_getValue
 // @grant       GM_setValue
 // @grant       GM_deleteValue
